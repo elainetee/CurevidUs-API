ceab3fd7ceafd227fdb3ddb3d303b6873e8dc2b8
meitingyau <meitingyau0240@gmail.com>
2022-12-24 01:51:43 +0800

    edit medicine photo, not yet done, error 500

--- app/Http/Controllers/MedicineController.php
+++ app/Http/Controllers/MedicineController.php
@@ -40,7 +40,7 @@ class MedicineController extends Controller
             $med->medicine_price = $request->get('medicine_price');
             $med->save();
 
-            return response()->json(['success' => 'File uploaded successfully.']);
+            return response()->json(['success' => 'Medicine created successfully.']);
         } else {
 
             $medicine = Medicine::create([
@@ -56,9 +56,43 @@ class MedicineController extends Controller
 
     public function update(Request $request, $medicine_id)
     {
+        // $request->validate([
+        //     'medicine_name' => 'required',
+        //     'medicine_desc' => 'required',
+        //     'medicine_price' => 'required',
+        //     'file' => 'mimes:jpg,jpeg,png|max:2048'
+        // ]);
+
         $medicine = Medicine::find($medicine_id);
-        $medicine->update($request->all());
-        return $medicine;
+
+        if ($request->file()) {
+            $file_name = time() . '_' . $request->file->getClientOriginalName();
+            $file_path = $request->file('file')->storeAs('uploads', $file_name, 'public');
+
+            // $medicine->medicine_photo_name = time() . '_' . $request->file->getClientOriginalName();
+            // $medicine->medicine_photo_path = '/storage/' . $file_path;
+            // $medicine->medicine_name = $request->get('medicine_name');
+            // $medicine->medicine_desc = $request->get('medicine_desc');
+            // $medicine->medicine_price = $request->get('medicine_price');
+            // $medicine->save();
+            $med['medicine_photo_name'] = time() . '_' . $request->file->getClientOriginalName();
+            $med['medicine_photo_path'] = '/storage/' . $file_path;
+            $med['medicine_name'] = $request->get('medicine_name');
+            $med['medicine_desc'] = $request->get('medicine_desc');
+            $med['medicine_price'] = $request->get('medicine_price');
+
+            $medicine->update($med);
+
+            return response()->json(['success' => 'Medicine updated successfully.']);
+        } else {
+            $medicine->update([
+                'medicine_name' => $request->get('medicine_name'),
+                'medicine_desc' => $request->get('medicine_desc'),
+                // 'medicine_photo' => $request->get('medicine_photo'),
+                'medicine_price' => $request->get('medicine_price')
+            ]);
+            return $medicine;
+        }
     }
 
     public function delete($medicine_id)
