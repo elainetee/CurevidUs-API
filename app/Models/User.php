7bcc8bf93e784db56448dd040256f8d46681ae3c
meitingyau <meitingyau0240@gmail.com>
2023-01-27 01:45:50 +0800

    after merge from main

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Staudenmeir\LaravelMergedRelations\Eloquent\HasMergedRelationships;
 class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable
-    // , HasMergedRelationships
+    , HasMergedRelationships
     ;
 
     /**
