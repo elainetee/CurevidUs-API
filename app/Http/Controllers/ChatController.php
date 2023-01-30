8d75973c6570eb1c51f234a218845a2eb41a2871
elainetee <elainetee88@gmail.com>
2023-01-30 09:24:24 +0800

    update on chat

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use App\Events\MessageSent;
 use App\Events\PrivateMessageSent;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Crypt;
 
 class ChatController extends Controller
@@ -18,8 +19,8 @@ class ChatController extends Controller
         // $message = Message::where('user_id', $user_id)->orderBy('id', 'DESC')->get();
         $messages = PublicRoom::get();
         foreach ($messages as $m) {
-            $decrypt=Crypt::decryptString($m->message);
-            $m->message=$decrypt;
+            $decrypt = Crypt::decryptString($m->message);
+            $m->message = $decrypt;
             $m->setAttribute('user_name', $m->user->userName());
         }
         return $messages;
@@ -34,15 +35,18 @@ class ChatController extends Controller
     {
         $user = JWTAuth::user();
         $encrypted = Crypt::encryptString($request->input('message'));
-        $message = $user->publicRooms()->create([
-            'message' => $request->input('message')
-        ]);
+        // dd($user->publicRooms());
         $messagedb = $user->publicRooms()->create([
             'message' => $encrypted
         ]);
-        broadcast(new MessageSent($message->load('user')))->toOthers();
-        return response(['status' => 'Message private sent successfully', 'message' => $message]);
 
+        $message = $user->publicRooms()->create([
+            'message' => $request->input('message')
+        ]);
+
+        broadcast(new MessageSent($message->load('user')))->toOthers();
+        $message->delete();
+        return response(['status' => 'Message private sent successfully', 'message' => $message]);
     }
 
     public function sendPrivateMessage(Request $request, $id)
@@ -53,9 +57,9 @@ class ChatController extends Controller
         //         'receiver_id' => $user->id
         //     ]);
         // }else{
-        $input=$request->all();
-        $input['receiver_id']=$id;
-        $message=JWTAuth::user()->messages()->create($input);
+        $input = $request->all();
+        $input['receiver_id'] = $id;
+        $message = JWTAuth::user()->messages()->create($input);
 
         // encrypt
         // $input = $request->all();
@@ -67,6 +71,7 @@ class ChatController extends Controller
         ]);
 
         broadcast(new PrivateMessageSent($message->load('user')))->toOthers();
+        $message->delete();
 
         return response(['status' => 'Message private sent successfully', 'message' => $message]);
     }
@@ -82,15 +87,15 @@ class ChatController extends Controller
         $user = User::find($id)->name;
         // dd($user);
         $privateCommunication = Message::where(['user_id' => JWTAuth::user()->id, 'receiver_id' => $id])
-        ->orWhere(function ($query) use ($id) {
-            $query->where(['user_id' => $id, 'receiver_id' => JWTAuth::user()->id]);
-        })
-        ->get();
+            ->orWhere(function ($query) use ($id) {
+                $query->where(['user_id' => $id, 'receiver_id' => JWTAuth::user()->id]);
+            })
+            ->get();
         foreach ($privateCommunication as $m) {
-            $decrypt=Crypt::decryptString($m->message);
+            $decrypt = Crypt::decryptString($m->message);
             $m->setAttribute('user_name', $m->user->userName());
             $m->setAttribute('avatar', $m->user->avatar());
-            $m->message=$decrypt;
+            $m->message = $decrypt;
         }
         return $privateCommunication;
     }
